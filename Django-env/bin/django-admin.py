af994737431aaaeb38bb0155ed28ce10a534b133
AlejandroCN7 <alejandroac79@gmail.com>
2018-11-07 13:21:42 +0100

    Prueba de despliegue

---
+++ Django-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hapneck/Escritorio/Universidad/CC/proyectos/Ejercicios_CC/Django-env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
